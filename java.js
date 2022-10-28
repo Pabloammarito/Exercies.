07099714cff82b10ac4362fad4e6e12869ca4e6b
Pabloammarito <mahmoodammar80@gmail.com>
2022-10-28 09:30:00 +0200

    ny version.

--- java.js
+++ java.js
@@ -103,6 +103,8 @@
 
    //Exercise 4 - level 2
 //-- Det jeg startet med og det virket ikke
+
+
    /*fetch("https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json")
        .then(response => response.json())
        .then(Movies => {
